c5c7ca742038d3bc7e22dd71a6cd10e56acf3a3a
PauloSuriani <paulosuriani@gmail.com>
2022-03-24 11:15:38 -0300

    feat: req-01-GET /talker

--- index.js
+++ index.js
@@ -5,13 +5,26 @@ const app = express();
 app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
-const PORT = '3000';
+const HTTP_ERROR_STATUS = 404;
 
-// não remova esse endpoint, e para o avaliador funcionar
+// Garante que o acesso será fixo na porta: 3000
+const APP_TRUST_PORT = '3000';
+
+const fs = require('fs').promises;
+
+app.get('/talker', (req, res) => {
+  try { 
+    const talkerList = fs.readFile('./talker.json', 'utf8');
+    return res.status(HTTP_OK_STATUS).json(JSON.parse(talkerList));
+  } catch (error) {
+    return res.status(HTTP_ERROR_STATUS).json([]);
+  }
+});
+
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
 
-app.listen(PORT, () => {
+app.listen(APP_TRUST_PORT, () => {
   console.log('Online');
 });
