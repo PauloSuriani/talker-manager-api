868b96d498987acf786377b771137e8a64ee8d16
PauloSuriani <paulosuriani@gmail.com>
2022-03-24 11:47:17 -0300

    fix: req-01 async refactor

--- index.js
+++ index.js
@@ -5,20 +5,27 @@ const app = express();
 app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
-const HTTP_ERROR_STATUS = 404;
+// const HTTP_ERROR_STATUS = 404;
 
 // Garante que o acesso será fixo na porta: 3000
 const APP_TRUST_PORT = '3000';
 
 const fs = require('fs').promises;
 
-app.get('/talker', (req, res) => {
-  try { 
-    const talkerList = fs.readFile('./talker.json', 'utf8');
-    return res.status(HTTP_OK_STATUS).json(JSON.parse(talkerList));
+const getTalkers = async () => {
+  try {
+      return await fs.readFile('./talker.json', 'utf-8');
   } catch (error) {
-    return res.status(HTTP_ERROR_STATUS).json([]);
+    return error;
   }
+};
+
+app.get('/talker', async (req, res) => {
+  const talkerList = await getTalkers();
+  if (!talkerList) {
+    return res.status(HTTP_OK_STATUS).json([]);
+  }
+  res.status(HTTP_OK_STATUS).json(JSON.parse(talkerList));
 });
 
 app.get('/', (_request, response) => {
