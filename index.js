c092bb8c53d95d19d76ad3cf10d420df786b569d
PauloSuriani <paulosuriani@gmail.com>
2022-03-24 12:56:20 -0300

    feat: req02-.get('/talker/:id...'

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
-// const HTTP_ERROR_STATUS = 404;
+const HTTP_ERROR_STATUS = 404;
 
 // Garante que o acesso será fixo na porta: 3000
 const APP_TRUST_PORT = '3000';
@@ -26,6 +26,22 @@ app.get('/talker', async (req, res) => {
     return res.status(HTTP_OK_STATUS).json([]);
   }
   res.status(HTTP_OK_STATUS).json(JSON.parse(talkerList));
+});
+
+app.get('/talker/:id', async (req, res) => {
+  const { id } = req.params;
+
+  const talkerList = await getTalkers();
+
+  if (!talkerList) {
+    return res.status(HTTP_OK_STATUS).json([]);
+  }
+
+  const talkerById = JSON.parse(talkerList).find((talkerSearch) => talkerSearch.id === +id);
+  if (!talkerById) {
+    return res.status(HTTP_ERROR_STATUS).json({ message: 'Pessoa palestrante não encontrada' });
+  }
+  res.status(HTTP_OK_STATUS).json(JSON.parse(talkerById));
 });
 
 app.get('/', (_request, response) => {
